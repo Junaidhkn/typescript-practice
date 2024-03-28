be7d3a5762c7ebea653f637ff25e54fc3e5805b7
junaidhkn <junaidhkniazi@gmail.com>
2024-03-28 23:24:17 +0500

    dictionary

--- packages/notes-ts-fundamentals-v4/src/04-objects-arrays-and-tuples.ts
+++ packages/notes-ts-fundamentals-v4/src/04-objects-arrays-and-tuples.ts
@@ -79,13 +79,20 @@ PrintObj({
 //* Index signatures
 
 //? Dictionary of phone #s
-const phones = {
+const phones: {
+  [k: string]: {
+    country: string
+    area: string
+    number: string
+  }
+} = {
   home: { country: '+1', area: '211', number: '652-4515' },
   work: { country: '+1', area: '670', number: '752-5856' },
   fax: { country: '+1', area: '322', number: '525-4357' },
 }
 
 //? Model as an index signature
+
 // const phones: {
 //     [k: string]: {
 //         country: string
